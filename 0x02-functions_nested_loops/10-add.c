cfd47ca15f7c4d31d33b0b150690ab2245b35885
Ecious <pmntsoane@gmail.com>
2023-07-19 10:17:11 -0700

    10-add git commit -m 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: Th first number to be computed
+ * @n2: The second number to be commputed
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+}
